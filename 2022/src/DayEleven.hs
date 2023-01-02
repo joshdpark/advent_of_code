e419e9b9b52c48c2e8f0335a304dbf2fcd6e4418
joshdpark <joshua.park@clarifyhealth.com>
2023-01-02 17:46:28 -0500

    day 11

---
+++ 2022/src/DayEleven.hs
@@ -0,0 +1,147 @@
+module DayEleven where
+
+import Data.Char (digitToInt, isNumber, isSymbol)
+import Data.List (foldl', sort)
+import qualified Data.Map as M
+import qualified Data.Sequence as S
+import qualified Data.Text as T
+import Text.Parsec (Parsec, char, digit, eof, letter, many, newline, parse, sepBy, space, spaces, string, (<|>))
+
+newtype Worry = Worry Integer deriving (Show, Eq)
+
+newtype Counter = Counter
+  {unCounter :: Word}
+  deriving (Show)
+
+data Monkey = Monkey
+  { monkeyItems :: S.Seq Worry,
+    monkeyOperation :: Worry -> Worry,
+    monkeyTest :: Worry -> Integer,
+    monkeyCounts :: Counter
+  }
+
+-- monkeyParser :: Parsec String () Monkey
+monkeyParser = go
+  where
+    indexParse = string "Monkey " *> digit <* char ':' <* newline
+    itemsParse = do
+      spaces *> string "Starting items: "
+      l <- sepBy (many digit) (char ',' *> spaces)
+      newline
+      return (fmap read l :: [Integer])
+    operationParse = do
+      op <-
+        spaces
+          *> string "Operation: new = old "
+          *> (char '*' <|> char '+')
+      num <- space *> many digit
+      return (op, num)
+    testParse = do
+      div' <- many (space <|> letter <|> space <|> char ':') *> many digit
+      true' <- many (space <|> letter <|> space <|> char ':') *> many digit
+      false' <- many (space <|> letter <|> space <|> char ':') *> many digit
+      newline
+      return ((read div', read true', read false') :: (Integer, Integer, Integer))
+    go = (,,,) <$> indexParse <*> itemsParse <*> operationParse <*> testParse
+
+inc :: Counter -> Counter
+inc (Counter a) = Counter (a + 1)
+
+operate :: (Integer -> Integer) -> Worry -> Worry
+operate f (Worry a) = Worry (f a)
+
+-- bored :: Worry -> Worry
+-- bored (Worry a) = Worry (a `div` 3)
+
+bored :: Worry -> Worry
+bored (Worry a) = Worry (a `mod` 9699690)
+
+divisibleByThrow :: Integer -> (Integer, Integer) -> Worry -> Integer
+divisibleByThrow a (true, false) (Worry b)
+  | b `rem` a == 0 = true
+  | otherwise = false
+
+inspectItem :: M.Map Integer Monkey -> Integer -> (Worry, M.Map Integer Monkey)
+inspectItem map index = (pop', M.update (\_ -> Just monkey') index map)
+  where
+    popMonkeyItem :: Monkey -> (Worry, Monkey)
+    popMonkeyItem (Monkey (pop S.:<| items') op test c) = (pop, Monkey items' op test (inc c))
+    (pop', monkey') = popMonkeyItem (map M.! index)
+
+throwItem :: M.Map Integer Monkey -> Integer -> Worry -> M.Map Integer Monkey
+throwItem map index worry = M.adjust (appendMonkeyItem worry) index map
+  where
+    appendMonkeyItem :: Worry -> Monkey -> Monkey
+    appendMonkeyItem worry (Monkey items op test c) = Monkey (items S.|> worry) op test c
+
+processMonkey :: M.Map Integer Monkey -> Integer -> M.Map Integer Monkey
+processMonkey map index = case monkeyItems (map M.! index) of
+  S.Empty -> map
+  _ -> go'
+    where
+      (pop, map') = inspectItem map index
+      monkey@(Monkey items op test count) = map' M.! index
+      newworry = bored . op $ pop
+      throwto = test newworry
+      newmap = throwItem map' throwto newworry
+      go' = processMonkey newmap index
+
+monkeys =
+  M.fromList
+    [ (0, Monkey (S.fromList [Worry 79, Worry 98]) (operate (* 19)) (divisibleByThrow 23 (2, 3)) (Counter 0)),
+      (1, Monkey (S.fromList [Worry 54, Worry 65, Worry 75, Worry 74]) (operate (+ 6)) (divisibleByThrow 19 (2, 0)) (Counter 0)),
+      (2, Monkey (S.fromList [Worry 79, Worry 60, Worry 97]) (operate (^ 2)) (divisibleByThrow 13 (1, 3)) (Counter 0)),
+      (3, Monkey (S.fromList [Worry 74]) (operate (+ 3)) (divisibleByThrow 17 (0, 1)) (Counter 0))
+    ]
+
+monkeyBusiness :: M.Map Integer Monkey -> M.Map Integer Monkey
+monkeyBusiness map = foldl' processMonkey map (M.keys map)
+
+listItems :: M.Map Integer Monkey -> M.Map Integer (S.Seq Worry)
+listItems = fmap monkeyItems
+
+listCounters :: M.Map Integer Monkey -> M.Map Integer Counter
+listCounters = fmap monkeyCounts
+
+round20 map = foldl' (\a _ -> monkeyBusiness a) map [0 .. 19]
+
+round10k map = foldl' (\a _ -> monkeyBusiness a) map [0 .. 9999]
+
+levelOfMonkey :: M.Map Integer Monkey -> Word
+levelOfMonkey map = go
+  where
+    list = fmap (unCounter . snd) . M.toList . listCounters $ map
+    go = product . take 2 . reverse . sort $ list
+
+constructMonkey ::
+  Integer ->
+  [Integer] ->
+  (Integer -> Integer) ->
+  (Integer, Integer, Integer) ->
+  (Integer, Monkey)
+constructMonkey index list op (d, t, f) = (index, Monkey (S.fromList $ Worry <$> list) (operate op) (divisibleByThrow d (t, f)) (Counter 0))
+
+monkeys' =
+  M.fromList
+    [ constructMonkey 0 [98, 70, 75, 80, 84, 89, 55, 98] (* 2) (11, 1, 4),
+      constructMonkey 1 [59] (^ 2) (19, 7, 3),
+      constructMonkey 2 [77, 95, 54, 65, 89] (+ 6) (7, 0, 5),
+      constructMonkey 3 [71, 64, 75] (+ 2) (17, 6, 2),
+      constructMonkey 4 [74, 55, 87, 98] (* 11) (3, 1, 7),
+      constructMonkey 5 [90, 98, 85, 52, 91, 60] (+ 7) (5, 0, 4),
+      constructMonkey 6 [99, 51] (+ 1) (13, 5, 2),
+      constructMonkey 7 [98, 94, 59, 76, 51, 65, 75] (+ 5) (2, 3, 6)
+    ]
+
+main :: IO ()
+main = do
+  f <- readFile "data/day11"
+  let p = parse (sepBy monkeyParser (string "\n")) "day11" f
+  either print print p
+
+main' :: IO T.Text
+main' = do T.pack <$> readFile "data/day11"
+
+-- 14400119988: too high
+-- 13621684730: too high
+-- 13119526120
--- 2022/src/DayTen.hs
+++ 2022/src/DayTen.hs
@@ -1,36 +1,96 @@
 module DayTen where
 
 data Instruction = Noop | Addx
+  deriving (Show, Eq)
 
-type Cycle = Int
-type Value = Int
+newtype Register = Register Int
+  deriving (Show, Eq)
 
-parseline :: [String] -> (Instruction, Int -> Int)
-parseline ["noop"] = (Noop, (+ 0))
-parseline ("addx" : [x]) = (Addx, (+ read x))
+newtype Position = Position Int
+  deriving (Show, Eq)
 
-execute :: (Instruction, Int -> Int) -> (Value, Cycle) -> (Value, Cycle)
-execute (i, f) (v, c) = case i of
-    Noop -> (f v, c + 1)
-    Addx -> (f v, c + 2)
+data Pixel = Lit | Dark
+  deriving (Show, Eq)
+
+newtype Cycle = Cycle Int
+  deriving (Show, Eq)
+
+newtype Operation = Operation (Int -> Int)
+
+inc :: Cycle -> Cycle
+inc (Cycle a) = Cycle (a + 1)
+
+operate :: Register -> Operation -> Register
+operate (Register x) (Operation f) = Register (f x)
+
+parseline :: [String] -> (Instruction, Operation)
+parseline ["noop"] = (Noop, Operation id)
+parseline ("addx" : [x]) = (Addx, Operation (+ read x))
+
+-- keep a stack to keep track of cycles and instructions
+buildStack :: [String] -> [(Instruction, Operation)]
+buildStack = foldr acc []
+  where
+    acc = (:) . parseline . words
+
+execute' ::
+  Cycle ->
+  Register ->
+  [(Instruction, Operation)] ->
+  [(Cycle, Register, Pixel)]
+execute' cycle  register [] = []
+execute' cycle register ((inst, op) : xs) = go
+  where
+    cycle' = inc cycle
+    pixel = drawPixel register cycle
+    go = case inst of
+      Noop -> (cycle', register, pixel) : execute' cycle' register xs
+      Addx -> (cycle', register, pixel) : (cycle'', register, drawPixel register cycle') : execute' cycle'' register' xs
+        where
+          register' = operate register op
+          cycle'' = inc cycle'
+
+execute :: [(Instruction, Operation)] -> [(Cycle, Register, Pixel)]
+execute = execute' (Cycle 0) (Register 1)
 
 test = ["noop", "addx 3", "addx -5"]
-process lines= scanl f (1, 0) $ map words lines
+
+drawPixel :: Register -> Cycle -> Pixel
+drawPixel (Register r) (Cycle c)
+  | c' >= r - 1 && c' <= r + 1 = Lit
+  | otherwise = Dark
+    where
+      c' = rem c 40
+
+getCycles :: [Int] -> [(Cycle, Register, Pixel)] -> [(Int, Int)]
+getCycles i [] = []
+getCycles i ((Cycle c, Register r, _): xs)
+    | c `elem` i = (c, r) : getCycles i xs
+    | otherwise = getCycles i xs
+
+drawPixels :: [Pixel] -> String
+drawPixels [] = []
+drawPixels (pixel:xs) = go
   where
-    f a b = execute (parseline b) a
+    chr = case pixel of
+        Lit -> '#'
+        Dark -> '.'
+    go = chr : drawPixels xs
 
-indexCycle :: [(Value, Cycle)] -> Int -> Cycle -> Maybe Int
-indexCycle [] _ i = Nothing
-indexCycle (x@(v,c):xs) p i
-    | c >= i = Just (p * i)
-    | otherwise = indexCycle xs v i
-
+splitPixelLine :: String -> [String]
+splitPixelLine [] = []
+splitPixelLine str = go
+  where
+    (line, xs) = splitAt 40 str
+    go = line : splitPixelLine xs
 
 main = do
   instructions <- readFile "data/day10"
-  let result = process . lines $ instructions
-      cycles = [20, 60, 100, 140, 180, 220]
-      signals = map (indexCycle result 0) cycles
-  print $ fmap sum (sequence signals)
-
+  let stack = buildStack . lines $ instructions
+      cycles = execute stack
+      indices = [20, 60, 100, 140, 180, 220]
+      result = getCycles indices cycles
+      pixels = foldr (\(_,_,x) b -> x : b) [] cycles
+  print cycles
+  mapM putStrLn (splitPixelLine . drawPixels $ pixels)
 
